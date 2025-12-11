d3c1334e6fbdbccbb9a7a173672f7cb4afae4447
Name <example@gmail.com>
2025-12-11 18:45:36 +0300

    push for some shit

--- api/create-payment.js
+++ api/create-payment.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end(); 
+    return res.status(200).end();
   }
 
   if (req.method !== "POST") {
